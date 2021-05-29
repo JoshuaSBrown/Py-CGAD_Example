bebcb998b4779beaec0208b0b9a96ae46b463482
JoshuaSBrown <joshua.s.brown@lanl.gov>
2021-05-28 21:54:41 -0600

    Add post_status app

---
+++ post_status.py
@@ -0,0 +1,43 @@
+
+import py-cgad.GitHubApp
+
+class PostStatusApp(GitHubApp):
+
+    def __init__(self):
+        super().__init__(117711,'StatusApp','lanl','Py-CGAD')
+
+
+def main(**kwargs):
+
+    app = PostStatusApp()
+    app.initialize(pem_file=kwargs['permissions'])
+    app.postStatus(kwargs['status'])
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser("Example implementation of Py-CGAD, will post generic message to provided commit hash")
+
+    desc = ('What is the state of the status: success, failed, error, pending')
+
+    parser.add_argument(
+            '--status',
+            '-s',
+            type=str,
+            nargs=1,
+            required=True,
+            help=desc)
+
+    desc = ('Permissions file, allows us to interact with the github repository')
+
+    parser.add_argument(
+            '--permissions',
+            '-p',
+            type=str,
+            nargs=1,
+            required=True,
+            help=desc)
+
+    args = parser.parse_args()
+
+    main(**vars(args))
