bc2f7b25b54778b933156d553285c73d7cd37dcb
par-hermes <par-hermes@automation.bot>
2021-06-24 05:46:56 +0000

    cpp-py-formatter

--- bin/post_status.py
+++ bin/post_status.py
@@ -1,47 +1,34 @@
-
 import argparse
 from status.status_app import StatusApp
 
+
 def main(**kwargs):
 
-    app = StatusApp(kwargs['verbose'])
-    app.initialize(pem_file=kwargs['permissions'])
-    app.postStatus(kwargs['status'],context='Status App')
+    app = StatusApp(kwargs["verbose"])
+    app.initialize(pem_file=kwargs["permissions"])
+    app.postStatus(kwargs["status"], context="Status App")
     app.printStatus()
 
-if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser("Example implementation of Py-CGAD, will post generic message to provided commit hash")
+if __name__ == "__main__":
 
-    desc = ('What is the state of the status: success, failed, error, pending')
+    parser = argparse.ArgumentParser(
+        "Example implementation of Py-CGAD, will post generic message to provided commit hash"
+    )
 
-    parser.add_argument(
-            '--status',
-            '-s',
-            type=str,
-            nargs=1,
-            required=True,
-            help=desc)
+    desc = "What is the state of the status: success, failed, error, pending"
 
-    desc = ('Permissions file, allows us to interact with the github repository')
+    parser.add_argument("--status", "-s", type=str, nargs=1, required=True, help=desc)
+
+    desc = "Permissions file, allows us to interact with the github repository"
 
     parser.add_argument(
-            '--permissions',
-            '-p',
-            type=str,
-            nargs=1,
-            required=True,
-            help=desc)
+        "--permissions", "-p", type=str, nargs=1, required=True, help=desc
+    )
 
-    desc = ('Vebosity of output.')
+    desc = "Vebosity of output."
 
-    parser.add_argument(
-            '--verbose',
-            '-v',
-            type=int,
-            nargs=1,
-            default=0,
-            help=desc)
+    parser.add_argument("--verbose", "-v", type=int, nargs=1, default=0, help=desc)
 
     args = parser.parse_args()
 
