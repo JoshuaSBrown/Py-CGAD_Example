530668ac0e9c03f56b2f4e592552c9ac89b34ff6
JoshuaSBrown <joshbro42867@yahoo.com>
2021-06-24 15:25:48 -0600

    Fix issues

--- tests/test_post_status.py
+++ tests/test_post_status.py
@@ -29,8 +29,6 @@ def test_get_status(test_app):
     commit_test_pending = "94eed4b8ffbfae827c42f0a8950ddb4e5c75c250"
     # Should only be a single status for this commit
     data_pending, code, _ = test_app.getStatuses(commit_test_pending)
-    print(json.dumps(data_pending, indent=4))
-    print(code)
     assert len(data_pending) == 1
     assert data_pending[0]["state"] == "pending"
     assert data_pending[0]["context"] == "Status App Test"
@@ -42,8 +40,6 @@ def test_get_status(test_app):
     commit_test_error = "2c7d52e095be03b896f380b86bd036990d6ffcc8"
     # Should only be a single status for this commit
     data_error, code, _ = test_app.getStatuses(commit_test_error)
-    print(json.dumps(data_error, indent=4))
-    print(code)
     assert len(data_error) == 1
     assert data_error[0]["state"] == "error"
     assert data_error[0]["context"] == "Status App Test"
@@ -55,8 +51,6 @@ def test_get_status(test_app):
     commit_test_success = "9f5c45fd76e1b17e1076d7226642e819e4b59aa1"
     # Should only be a single status for this commit
     data_success, code, _ = test_app.getStatuses(commit_test_success)
-    print(json.dumps(data_success, indent=4))
-    print(code)
     assert len(data_success) == 1
     assert data_success[0]["state"] == "success"
     assert data_success[0]["context"] == "Status App Test"
@@ -71,7 +65,7 @@ def test_post_status(test_app):
     # This is because of the ci we are testing different python version in
     # parallel and we don't want to overwrite changes to the status
     commit_for_python_version3_7 = "5ac6e084ccdfd9375537a485345f2909b748648e"
-    commit_for_python_version3_8 = "a76b68983e42255589cda8b4de47133aef22c984"
+    commit_for_python_version3_8 = "e09b36e3a33c9f857ac275fdb9fb0445a42f245f"
     commit_for_python_version3_9 = "7aa7fdc0871c6d8484f47050f6e839e9b7ee4691"
 
     test_commit = commit_for_python_version3_7
@@ -80,8 +74,6 @@ def test_post_status(test_app):
             test_commit = commit_for_python_version3_8
         elif sys.version_info[1] == 9:
             test_commit = commit_for_python_version3_9
-
-    print("test commit is {}".format(test_commit))
 
     test_app.postStatus("pending", commit_sha=test_commit)
     # Will return the latest state which should now be pending
